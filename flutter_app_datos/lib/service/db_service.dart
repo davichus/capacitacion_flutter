980c966dbbb9068b0432f3eb1d8c978e0f6af83c
davidnavarrete <davidnava1998efrain@outlook.com>
2026-03-27 19:51:20 -0500

    semana 7
    service db

---
+++ flutter_app_datos/lib/service/db_service.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_app_datos/models/user_model.dart';
+import 'package:sqflite/sqflite.dart';
+class DbService{
+  static final DbService _instance = DbService._internal();
+  factory DbService() => _instance;
+  DbService._internal();
+  static Database? _database;
+
+  Future<Database> get database async{
+    if(_database != null) return _database!;
+    _database = await _initDb();
+    return _database!;
+  }
+
+  Future<Database> _initDb() async{
+    String path = '${await getDatabasesPath()}app_user.db';
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async{
+        await db.execute('''
+            CREATE TABLE users(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT NOT NULL,
+              email TEXT NOT NULL,
+              password TEXT NOT NULL
+            )''');
+      });
+  }
+
+  Future<int> insertUser(User user) async{
+    final db = await database;
+    return await db.insert('users', user.toMap());
+  }
+
+  Future<List<User>> getUsers() async {
+    final db = await database;
+    final res = await db.query('users');
+    return res.map((e) => User.fromMap(e)).toList();
+  }
+}
